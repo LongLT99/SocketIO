427244d0dcfb473b6514a49ec0b042324556fd6e
LongLT99 <59241152+LongLT99@users.noreply.github.com>
2020-02-03 17:39:14 +0700

    video and audio up date

--- public/client.js
+++ public/client.js
@@ -1,46 +1,69 @@
-const constraints = window.constraints = {
-    audio: true,
-    video: true
-  };
-  
-  function handleSuccess(stream) {
-    const video = document.querySelector('video');
-    const videoTracks = stream.getVideoTracks();
-    console.log('Got stream with constraints:', constraints);
-    console.log(`Using video device: ${videoTracks[0].label}`);
-    window.stream = stream; // make variable available to browser console
-    video.srcObject = stream;
-  }
-  
-  function handleError(error) {
-    if (error.name === 'ConstraintNotSatisfiedError') {
-      const v = constraints.video;
-      errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
-    } else if (error.name === 'PermissionDeniedError') {
-      errorMsg('Permissions have not been granted to use your camera and ' +
-        'microphone, you need to allow the page access to your devices in ' +
-        'order for the demo to work.');
-    }
-    errorMsg(`getUserMedia error: ${error.name}`, error);
-  }
-  
-  function errorMsg(msg, error) {
-    const errorElement = document.querySelector('#errorMsg');
-    errorElement.innerHTML += `<p>${msg}</p>`;
-    if (typeof error !== 'undefined') {
-      console.error(error);
-    }
-  }
-  
-  async function init() {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      handleSuccess(stream);
-      e.target.disabled = true;
-  }
+const constraints = (window.constraints = {
+  audio: true,
+  video: true
+});
 
-  async function endvideo(){
-    constraints ={
-      audio: false,
-      video: false
-    };
+function handleSuccess(stream) {
+  const videoTracks = stream.getTracks();
+  console.log("Got stream with constraints:", constraints);
+  console.log(`Using video device: ${videoTracks[0].label}`);
+  window.stream = stream; // make variable available to browser console
+  localvideo.srcObject = stream;
+}
+
+function handleError(error) {
+  if (error.name === "ConstraintNotSatisfiedError") {
+    const v = constraints.video;
+    errorMsg(
+      `The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`
+    );
+  } else if (error.name === "PermissionDeniedError") {
+    errorMsg(
+      "Permissions have not been granted to use your camera and " +
+        "microphone, you need to allow the page access to your devices in " +
+        "order for the demo to work."
+    );
   }
+  errorMsg(`getUserMedia error: ${error.name}`, error);
+}
+
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector("#errorMsg");
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== "undefined") {
+    console.error(error);
+  }
+}
+
+async function init(e) {
+  try {
+    const streams = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(streams);
+    e.target.disabled = true;
+  } catch (e) {
+    handleError(e);
+  }
+}
+const localvideo = document.querySelector("video#local_video");
+const vb = document.querySelector("button#video_button");
+if (vb) {
+  vb.addEventListener("click", e => init(e));
+}
+
+$("#modal_id").on("hidden.bs.modal", function() {
+    const stream = localvideo.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(function(track) {
+      track.stop();
+    });
+    $("#video_button").prop('disabled', false);
+});
+
+const peer = new Peer({key: 'lwjd5qra8257b9'});
+
+peer.on('open', id => console.log(id) ,function(){
+  console.log("hle"+id);
+  
+});
+
+
